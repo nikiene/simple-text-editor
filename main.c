db6be18c5515b3e554ffd59565312b3bb6bb0cc2
nikiene <nikolasnovo03@gmail.com>
2024-05-02 20:16:31 -0300

    feat: writing input to 'c' variable

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
-int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
 {
-    
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+        ;
+
     return 0;
 }
