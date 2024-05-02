fd4581e3b65ca6f3a49a9269be37e68c5496367f
nikiene <nikolasnovo03@gmail.com>
2024-05-02 20:30:27 -0300

    feat: diabling raw mode on exit

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode()
 {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO | ICANON);
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
