72584a9b74a3314b2ba87185acc64d677d644a1d
nikiene <nikolasnovo03@gmail.com>
2024-05-02 20:26:25 -0300

    feat: adding enablerawmode function

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+
+void enableRawMode()
+{
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
+    enableRawMode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
         ;
