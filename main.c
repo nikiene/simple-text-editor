782daad35f690e9683ec43001ad679488455c8d0
nikiene <nikolasnovo03@gmail.com>
2024-05-16 15:31:28 -0300

    chore: Improve cursor movement with arrow keys

--- main.c
+++ main.c
@@ -11,13 +11,25 @@
 
 /*** defines ***/
 
+#define EDITOR_VERSION "0.0.1"
+
 // ctrl-key macro to bitwise-AND the key with 0x1f (00011111) to get the ASCII value of the ctrl-key
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+enum editorKey
+{
+    ARROW_LEFT = '1000',
+    ARROW_RIGHT, // 1001
+    ARROW_UP,    // 1002
+    ARROW_DOWN   // 1003
+};
 
 /*** data ***/
 
 struct editorConfig
 {
+    int cx, cy;
+
     int screenrows;
     int screencols;
 
@@ -31,7 +43,10 @@ struct editorConfig E;
 // prints an error message and exits the program with status 1
 void die(const char *s)
 {
+    // [2J - clear entire screen
     write(STDOUT_FILENO, "\x1b[2J", 4);
+
+    // H - position cursor
     write(STDOUT_FILENO, "\x1b[H", 3);
 
     perror(s);
@@ -95,7 +110,7 @@ void enableRawMode()
 }
 
 // wait a keypress and return it
-char editorReadKey()
+int editorReadKey()
 {
     int nread;
     char c;
@@ -103,6 +118,36 @@ char editorReadKey()
     {
         if (nread == -1 && errno != EAGAIN)
             die("read");
+    }
+
+    if (c == '\x1b')
+    {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+
+        if (seq[0] == '[')
+        {
+            switch (seq[1])
+            {
+            case 'A':
+                return ARROW_UP;
+            case 'B':
+                return ARROW_DOWN;
+            case 'C':
+                return ARROW_RIGHT;
+            case 'D':
+                return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    }
+    else
+    {
+        return c;
     }
 
     return c;
@@ -114,7 +159,6 @@ int getCursorPosition(int *rows, int *cols)
     unsigned int i = 0;
 
     // escape sequence to query the cursor position
-    // \x1b - escape character
     // [6n - query cursor position
     if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
         return -1;
@@ -149,7 +193,6 @@ int getWindowSize(int *rows, int *cols)
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
     {
         // escape sequence to position the cursor at the bottom-right corner of the screen
-        // \x1b - escape character
         // [999C - move cursor 999 characters to the right
         // [999B - move cursor 999 characters down
 
@@ -205,7 +248,31 @@ void editorDrawRows(struct abuf *ab)
 {
     for (int y = 0; y < E.screenrows; y++)
     {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                                      "simple text editor -- version %s", EDITOR_VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+            abAppend(ab, "~", 1);
+        }
+
+        // [K - erase in line
+        abAppend(ab, "\x1b[K", 3);
+
         if (y < E.screenrows - 1)
         {
             abAppend(ab, "\r\n", 2);
@@ -217,27 +284,54 @@ void editorRefreshScreen()
 {
     struct abuf ab = ABUF_INIT;
 
-    // \x1b - escape character
-    // [2J - clear entire screen
-    abAppend(&ab, "\x1b[2J", 4);
+    // [?25l - hide cursor
+    abAppend(&ab, "\x1b[?25l", 6);
 
-    // \x1b - escape character
     // H - position cursor
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    abAppend(&ab, "\x1b[?25h", 6);
+
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
 }
 
 /*** input ***/
 
+// moves the cursor based on the keypress
+void editorMoveCursor(int key)
+{
+    switch (key)
+    {
+    case ARROW_LEFT:
+        if (E.cx != 0)
+            E.cx--;
+        break;
+    case ARROW_RIGHT:
+        if (E.cx != E.screencols - 1)
+            E.cx++;
+        break;
+    case ARROW_UP:
+        if (E.cy != 0)
+            E.cy--;
+        break;
+    case ARROW_DOWN:
+        if (E.cy != E.screenrows - 1)
+            E.cy++;
+        break;
+    }
+}
+
 // waits for a keypress and handles it
 void editorProcessKeypress()
 {
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch (c)
     {
@@ -246,6 +340,13 @@ void editorProcessKeypress()
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+        editorMoveCursor(c);
+        break;
     }
 }
 
@@ -253,6 +354,9 @@ void editorProcessKeypress()
 
 void initEditor()
 {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
