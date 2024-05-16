87e7771a2e50307b3cae4a39591697115fc56a7c
nikiene <nikolasnovo03@gmail.com>
2024-05-16 19:30:02 -0300

    feat: Improve cursor movement and scrolling functionality

--- main.c
+++ main.c
@@ -48,6 +48,7 @@ typedef struct editorRow
 struct editorConfig
 {
     int cx, cy;
+    int rx;
 
     int rowoff;
     int coloff;
@@ -277,6 +278,19 @@ int getWindowSize(int *rows, int *cols)
 
 /*** row operations ***/
 
+// converts the index of a character in a row from the cx (character index) to the rx (render index)
+void editorRowCxToRx(erow *row, int cx)
+{
+    int rx = 0;
+
+    for (int j = 0; j < cx; j++)
+    {
+        if (row->chars[j] == '\t')
+            rx += (EDITOR_TAB_STOP - 1) - (rx % EDITOR_TAB_STOP);
+        rx++;
+    }
+}
+
 void editorUpdateRow(erow *row)
 {
     int tabs = 0;
@@ -388,6 +402,12 @@ void abFree(struct abuf *ab)
 // scrolls the editor to keep the cursor in view
 void editorScroll()
 {
+    E.rx = 0;
+    if (E.cy < E.numrows)
+    {
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+    }
+
     if (E.cy < E.rowoff)
     {
         E.rowoff = E.cy;
@@ -396,13 +416,13 @@ void editorScroll()
     {
         E.rowoff = E.cy - E.screenrows + 1;
     }
-    if (E.cx < E.coloff)
+    if (E.rx < E.coloff)
     {
-        E.coloff = E.cx;
+        E.coloff = E.rx;
     }
-    if (E.cx >= E.coloff + E.screencols)
+    if (E.rx >= E.coloff + E.screencols)
     {
-        E.coloff = E.cx - E.screencols + 1;
+        E.coloff = E.rx - E.screencols + 1;
     }
 }
 
@@ -475,7 +495,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -573,6 +593,7 @@ void initEditor()
 {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
 
     E.rowoff = 0;
     E.coloff = 0;
