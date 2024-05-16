7bd587a8a1f0cdecac83508c7f96d1bddfabf047
nikiene <nikolasnovo03@gmail.com>
2024-05-16 15:42:50 -0300

    chore: add support for home, end, page up, and page down keys

--- main.c
+++ main.c
@@ -21,7 +21,11 @@ enum editorKey
     ARROW_LEFT = '1000',
     ARROW_RIGHT, // 1001
     ARROW_UP,    // 1002
-    ARROW_DOWN   // 1003
+    ARROW_DOWN,  // 1003
+    HOME_KEY,    // 1004
+    END_KEY,     // 1005
+    PAGE_UP,     // 1006
+    PAGE_DOWN,   // 1007
 };
 
 /*** data ***/
@@ -131,26 +135,65 @@ int editorReadKey()
 
         if (seq[0] == '[')
         {
-            switch (seq[1])
+            if (seq[1] >= '0' && seq[1] <= '9')
             {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '1':
+                        return HOME_KEY;
+                    case '4':
+                        return END_KEY;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    case '7':
+                        return HOME_KEY;
+                    case '8':
+                        return END_KEY;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+                }
             }
         }
+        else if (seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+            case 'H':
+                return HOME_KEY;
+            case 'F':
+                return END_KEY;
+            }
+        }
+
         return '\x1b';
     }
     else
     {
         return c;
     }
-
-    return c;
 }
 
 int getCursorPosition(int *rows, int *cols)
@@ -340,6 +383,23 @@ void editorProcessKeypress()
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+        
+    case END_KEY:
+        E.cx = E.screencols - 1;
+        break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        int times = E.screenrows;
+        while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+    break;
 
     case ARROW_UP:
     case ARROW_DOWN:
