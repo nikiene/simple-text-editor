85b026cf0dfb30c6d3dac9a0e49a1476ee55b94f
nikiene <nikolasnovo03@gmail.com>
2024-05-16 19:12:10 -0300

    feat: Improve cursor movement and scrolling functionality

--- main.c
+++ main.c
@@ -457,6 +457,11 @@ void editorMoveCursor(int key)
     case ARROW_LEFT:
         if (E.cx != 0)
             E.cx--;
+        else if (E.cy > 0)
+        {
+            E.cy--;
+            E.cx = E.row[E.cy].size;
+        }
         break;
     case ARROW_RIGHT:
         if (row && E.cx < row->size)
