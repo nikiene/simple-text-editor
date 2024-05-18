91be5cecc34c12887d63402ab31e23ba3b0eefe4
nikiene <nikolasnovo03@gmail.com>
2024-05-17 21:33:24 -0300

    feat: add character insertion functionality to the editor

--- main.c
+++ main.c
@@ -348,6 +348,33 @@ void editorAppendRow(char *s, size_t len)
     E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c)
+{
+    if (at < 0 || at > row->size)
+        at = row->size;
+
+    row->chars = realloc(row->chars, row->size + 2);
+
+    // shift the characters to the right of the cursor to the right by one
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++;
+
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+// inserts a character at the cursor position
+void editorInsertChar(int c)
+{
+    if (E.cy == E.numrows)
+        editorAppendRow("", 0);
+
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 /*** file i/o ***/
 
 // opens a file and reads it line by line appending each line to the editor
@@ -659,6 +686,10 @@ void editorProcessKeypress()
     case ARROW_LEFT:
     case ARROW_RIGHT:
         editorMoveCursor(c);
+        break;
+
+    default:
+        editorInsertChar(c);
         break;
     }
 }
